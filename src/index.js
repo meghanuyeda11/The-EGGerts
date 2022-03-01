3a6c9894dbece366499ecc23f3f912b4e8465929
Jeffrey Loewe <jeffreyloewe123@gmail.com>
2022-03-01 12:06:55 -0800

    added row entry and game logic

--- src/index.js
+++ src/index.js
@@ -7,39 +7,82 @@ import useLogin from './components/useLogin';
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: "",
-      typed: false
-    }
-    this.enterCharacter = this.enterCharacter.bind(this);
   }
 
-  enterCharacter(event) {
+  clearCharacter() {
     if (!this.state.typed) { // if haven't typed
-      this.setState({value: event.key.toUpperCase(), typed: true});
+      this.setState({value: "", typed: false});
     }
   }
 
   render() {
     return (
-      <button className="square" onKeyPress={this.enterCharacter}>
-        {this.state.value}
+      <button className="square">
+        {this.props.tileContent}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square 
-    />;
+  constructor() {
+    super();
+    this.focusRef = React.createRef();
+    this.enterCharacter = this.enterCharacter.bind(this);
+    this.delCharacter = this.delCharacter.bind(this);
+    this.state = {
+      currentCell: 0,
+      currentRow: 1,
+      cellVals: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+      "", "", "", "", "", "", "", "", "", ""]
+    };
+  }
+
+  enterCharacter(event) { // new css focus class
+    if (event.key === 'Enter') {
+      if (this.state.currentCell == (this.state.currentRow*5)) { // reached end of row
+        var wordExists = true; // replace with function call
+        if (wordExists) { // word exists
+          
+          // check for correctness, otherwise cont.
+          
+          // change colors of current row
+
+
+          this.setState({currentRow: this.state.currentRow+1}); // increment the row
+        }
+      }
+    } else if (event.key === 'Backspace') {
+      this.delCharacter();
+    } else { // insert character into tile
+      if (this.state.currentCell < (this.state.currentRow*5)) {
+        const newVals = this.state.cellVals.slice(); // copy the array
+        newVals[this.state.currentCell] = event.key.toUpperCase(); 
+        this.setState({cellVals: newVals, currentCell: this.state.currentCell+1});
+      }
+    }
+  }
+
+  delCharacter() {
+    if (this.state.currentCell > (this.state.currentRow*5)-5) { // past or at beginning of row
+      const newVals = this.state.cellVals.slice(); // copy the array
+      //console.log(newVals[this.state.currentCell]);
+      newVals[this.state.currentCell-1] = "";
+      this.setState({cellVals: newVals, currentCell: this.state.currentCell-1});
+    }
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.focusRef.current.focus();
+    }, 1)
   }
 
   render() {
     const status = 'Wordle - UCLA Edition';
-
+             
     return (
-      <div>
+      <div tabIndex="0" ref={this.focusRef} onKeyDown={this.enterCharacter}>
         <div className="status">{status}</div>
         <div className="button-panel">
           <button>instructions</button>
@@ -47,49 +90,49 @@ class Board extends React.Component {
           <button>update leaderboard</button>
         </div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
+          <Square tileContent={this.state.cellVals[0]}/>
+          <Square tileContent={this.state.cellVals[1]}/>
+          <Square tileContent={this.state.cellVals[2]}/>
+          <Square tileContent={this.state.cellVals[3]}/>
+          <Square tileContent={this.state.cellVals[4]}/>
         </div>
         <div className="board-row">
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
+          <Square tileContent={this.state.cellVals[5]}/>
+          <Square tileContent={this.state.cellVals[6]}/>
+          <Square tileContent={this.state.cellVals[7]}/>
+          <Square tileContent={this.state.cellVals[8]}/>
+          <Square tileContent={this.state.cellVals[9]}/>
         </div>
         <div className="board-row">
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
+          <Square tileContent={this.state.cellVals[10]}/>
+          <Square tileContent={this.state.cellVals[11]}/>
+          <Square tileContent={this.state.cellVals[12]}/>
+          <Square tileContent={this.state.cellVals[13]}/>
+          <Square tileContent={this.state.cellVals[14]}/>
         </div>
         <div className="board-row">
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
+          <Square tileContent={this.state.cellVals[15]}/>
+          <Square tileContent={this.state.cellVals[16]}/>
+          <Square tileContent={this.state.cellVals[17]}/>
+          <Square tileContent={this.state.cellVals[18]}/>
+          <Square tileContent={this.state.cellVals[19]}/>
         </div>
         <div className="board-row">
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
+          <Square tileContent={this.state.cellVals[20]}/>
+          <Square tileContent={this.state.cellVals[21]}/>
+          <Square tileContent={this.state.cellVals[22]}/>
+          <Square tileContent={this.state.cellVals[23]}/>
+          <Square tileContent={this.state.cellVals[24]}/>
         </div>
         <div className="board-row">
-          {this.renderSquare(25)}
-          {this.renderSquare(26)}
-          {this.renderSquare(27)}
-          {this.renderSquare(28)}
-          {this.renderSquare(29)}
+          <Square tileContent={this.state.cellVals[25]}/>
+          <Square tileContent={this.state.cellVals[26]}/>
+          <Square tileContent={this.state.cellVals[27]}/>
+          <Square tileContent={this.state.cellVals[28]}/>
+          <Square tileContent={this.state.cellVals[29]}/>
         </div>
       </div>
-    );
+    ); 
   }
 }
 
