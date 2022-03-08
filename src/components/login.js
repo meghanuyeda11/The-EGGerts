c0be95d6dd86f0b1361076651b8e2c19e7d79f83
Jeffrey Loewe <jeffreyloewe123@gmail.com>
2022-03-08 14:10:22 -0800

    implemented backend logic for login

--- src/components/login.js
+++ src/components/login.js
@@ -1,57 +1,61 @@
 import React, { useState } from 'react';
 import './login.css'
 import PropTypes from 'prop-types';
-
+ 
 async function loginUser(credentials) {
-    return fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-
-    .then(data => data.json())
+   return fetch('http://localhost:8080/login', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(credentials)
+   })
+ 
+   .then(data => data.json())
 }
-
+ 
 function Login({ setLogin }) {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
-    
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-          username,
-          password
-        });
-        setLogin(token);
-    }
-
-    return (
-        <div className="login">
-            <div className = "login-inner">
-                <h1>Please Log In</h1>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                    <p>Username</p>
-                    <input type="text" onChange={e => setUserName(e.target.value)}/>
-                    </label>
-                    <label>
-                    <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)}/>
-                    </label>
-                    <div>
-                    <p></p>
-                    <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>            
-        </div>
-    );
+   const [username, setUserName] = useState();
+   const [password, setPassword] = useState();
+  
+   const handleSubmit = async e => {
+       e.preventDefault();
+       const token = await loginUser({
+         username,
+         password
+       });
+       setLogin(token);
+       if (localStorage.getItem(username) == null) { // if username does not exist
+           localStorage.setItem(username, -1) // add it
+       }
+       localStorage.setItem("currentUser", username) // set current username
+   }
+ 
+   return (
+       <div className="login">
+           <div className = "login-inner">
+               <h1>Please Log In</h1>
+               <form onSubmit={handleSubmit}>
+                   <label>
+                   <p>Username</p>
+                   <input type="text" onChange={e => setUserName(e.target.value)}/>
+                   </label>
+                   <label>
+                   <p>Password</p>
+                   <input type="password" onChange={e => setPassword(e.target.value)}/>
+                   </label>
+                   <div>
+                   <p></p>
+                   <button type="submit">Submit</button>
+                   </div>
+               </form>
+           </div>           
+       </div>
+   );
 }
-
+ 
 export default Login
-
+ 
 Login.propTypes = {
-    setLogin: PropTypes.func.isRequired
+   setLogin: PropTypes.func.isRequired
 }
